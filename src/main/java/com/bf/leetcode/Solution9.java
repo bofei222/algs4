baf8a881220325eb650f28c8eb1f8b17cc47fcd2
bofei <bofei222@163.com>
2020-06-09 18:22:53 +0800

    值传递 还是引用传递

--- src/main/java/com/bf/leetcode/Solution9.java
+++ src/main/java/com/bf/leetcode/Solution9.java
@@ -1,43 +1,22 @@
 package com.bf.leetcode;
 
 import com.sun.xml.internal.ws.addressing.WsaActionUtil;
+import edu.princeton.cs.algs4.In;
 import org.junit.Test;
 
 import java.util.Arrays;
 
 /**
  * @description:
+ * 为什么不是对int类型进行自动装箱处理呢？其实这块是java根据一个很明显的道理进行设计的：如果有人比较一个int类型的值和Integer类型的值，是想比较什么呢？肯定是值呀，所以这块是对Integer对象进行拆箱而不是对int类型装箱了
+ *
+ *
  * @author: bofei
  * @date: 2020-06-09 09:28
  **/
 public class Solution9 {
 
-    public static void main(String[] args) {
 
-        int i = 123321;
-        int j = 123321;
-        System.out.println(i == j);
-        System.out.println();
-
-        int xx = i;
-        System.out.println("xx == i " + (xx == i));
-        i--;
-        System.out.println("xx " + xx);
-        System.out.println("xx == i " + (xx == i));
-
-        System.out.println();
-        System.out.println("array=============");
-        String[] a = {"a", "b", "c"};
-        String[] b = {"a", "b", "c"};
-        System.out.println(a.equals(b)); // == 比较引用的值 -> 比较内存的物理地址
-
-
-        System.out.println("String==================");
-        String abc = new String("abv");
-        String abc2 = new String("abv");
-        System.out.println(abc == abc2);
-        System.out.println(abc.equals(abc2));
-    }
 
     @Test
     public void test() {
@@ -65,6 +44,67 @@ public class Solution9 {
         System.out.println(isPalindrome3(1221));
         System.out.println(isPalindrome3(-121));
         System.out.println(isPalindrome3(10));
+    }
+
+    @Test
+    public void test4() {
+        System.out.println(isPalindrome4(123321));
+//        System.out.println(isPalindrome4(1221));
+//        System.out.println(isPalindrome4(-121));
+//        System.out.println(isPalindrome4(10));
+    }
+
+    @Test
+    public void test5() {
+        System.out.println(isPalindrome5(123321));
+//        System.out.println(isPalindrome4(1221));
+//        System.out.println(isPalindrome4(-121));
+//        System.out.println(isPalindrome4(10));
+    }
+
+    // 一半 half ， 只比较 前一半 和后一半 即可
+    public static boolean isPalindrome5(int x) {
+        int oldX = x;
+        if (x < 0) return false;
+        if (x == 0) return true;
+        int l = (x + "").length(); // 3位数 2个零 123/100
+        int half = l / 2; // 奇数、偶数 都处理了
+
+        int loopCount = half - 1;
+        int result = 0;
+        for (; ; ) {
+            int r = x % 10;
+//            System.out.println(r);
+            x = x / 10;
+            result = result * 10 + r; // 乘几个零， 等价于 *10 乘几次  l-1个
+            System.out.println(result);
+            if (loopCount == 0) break;
+            loopCount--;
+        }
+
+//        System.out.println("result " + result);
+        return x == result;
+    }
+
+    public static boolean isPalindrome4(int x) {
+        int oldX = x;
+        if (x < 0) return false;
+        if (x == 0) return true;
+        int l = (x + "").length() - 1; // 3位数 2个零 123/100
+
+        int result = 0;
+        for (; ; ) {
+            int r = x % 10;
+//            System.out.println(r);
+            x = x / 10;
+            result = result * 10 + r; // 乘几个零， 等价于 *10 乘几次  l-1个
+            System.out.println(result);
+            if (l == 0) break;
+            l--;
+        }
+
+//        System.out.println("result " + result);
+        return oldX == result;
     }
 
     public static boolean isPalindrome3(int x) {
